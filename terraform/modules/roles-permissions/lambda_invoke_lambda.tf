89a28211b2d45a105191bea95001180df94bff6f
Thomas Moellers <thomas.moellers@rwth-aachen.de>
2021-06-27 09:11:23 +0800

    fix: Move lambda policy to json file

--- terraform/modules/roles-permissions/lambda_invoke_lambda.tf
+++ terraform/modules/roles-permissions/lambda_invoke_lambda.tf
@@ -3,18 +3,19 @@
 resource "aws_iam_role" "lambda_invoke_lambda_role" {
   name = "invoke_lambda_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-        Effect = "Allow"
-      },
-    ]
-  })
+  assume_role_policy = file("${path.module}/lambda_invoke_lambda_policy.json")
+  # assume_role_policy = jsonencode({
+  #   Version = "2012-10-17"
+  #   Statement = [
+  #     {
+  #       Action = "sts:AssumeRole"
+  #       Principal = {
+  #         Service = "lambda.amazonaws.com"
+  #       }
+  #       Effect = "Allow"
+  #     },
+  #   ]
+  # })
 }
 
 resource "aws_iam_policy" "lambda_invoke_lambda_policy" {
