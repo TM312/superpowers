6b197d4ad9d4ff619a5aa405e3ae2db4d69ce1d7
Thomas Moellers <thomas.moellers@rwth-aachen.de>
2021-07-23 11:38:08 +0800

    fix: Add layer to function

--- terraform/modules/lambda-functions/get_text_analysis.tf
+++ terraform/modules/lambda-functions/get_text_analysis.tf
@@ -3,8 +3,6 @@ data "archive_file" "zip_lambda_function_get_text_analysis" {
   type        = "zip"
   source_file = "${path.root}/../functions/nlp/get_text_analysis/handler.py"
   output_path = "${path.root}/../functions/nlp/get_text_analysis/handler.zip"
-
-  depends_on = [null_resource.get_text_analysis_buildstep]
 }
 
 
@@ -13,6 +11,7 @@ resource "aws_lambda_function" "lambda_get_text_analysis" {
   filename         = data.archive_file.zip_lambda_function_get_text_analysis.output_path
   source_code_hash = data.archive_file.zip_lambda_function_get_text_analysis.output_base64sha256
   function_name    = "lambda_get_text_analysis_${var.env}"
+  layers           = [var.layer_nlp_arn]
   role             = var.lambda_execute_role_arn
   handler          = "handler.lambda_handler"
   runtime          = "python3.8"
