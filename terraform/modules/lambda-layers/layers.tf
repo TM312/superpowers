9a5f5f59dd3d691e00675ba6f27e7ed36a747bf4
Thomas Moellers <thomas.moellers@rwth-aachen.de>
2021-07-23 14:51:14 +0800

    nit: Formatting

--- terraform/modules/lambda-layers/layers.tf
+++ terraform/modules/lambda-layers/layers.tf
@@ -1,8 +1,7 @@
 resource "aws_lambda_layer_version" "layer_nlp" {
-  description = "lambda layer to provide nlp/spacy deps"
-  s3_bucket   = aws_s3_bucket.s3_lambda_layers.bucket
-  s3_key      = aws_s3_bucket_object.nlp_layer.key
-  layer_name  = "layer_nlp"
-
+  description         = "lambda layer to provide nlp/spacy deps"
+  s3_bucket           = aws_s3_bucket.s3_lambda_layers.bucket
+  s3_key              = aws_s3_bucket_object.nlp_layer.key
+  layer_name          = "layer_nlp"
   compatible_runtimes = ["python3.8"]
 }
