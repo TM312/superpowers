624166a576a2f20e8e4a2ffa12aae9524148b92a
Thomas <thomas.moellers@rwth-aachen.de>
2021-08-11 09:32:12 +0800

    fix: Remove containerization of page

--- landing/pages/index.vue
+++ landing/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-12 lg:py-16 lg:px-16">
-        <section-landing-top class="mt-5 py-5" />
-        <section-landing-main class="mt-5 py-5" />
+    <div>
+        <section-landing-top />
+        <section-landing-main class="mt-5 py-5 px-5 lg:px-10" />
     </div>
 </template>
 
