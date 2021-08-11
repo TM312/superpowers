ff753565abf89b16ef75e4e7cfef0d3f31af27c7
Thomas <thomas.moellers@rwth-aachen.de>
2021-08-11 09:31:27 +0800

    feat: Initial styling hero section

--- landing/components/section/SectionLandingTop.vue
+++ landing/components/section/SectionLandingTop.vue
@@ -1,13 +1,34 @@
 <template>
     <section>
-        <div>
-            <h1 class="text text-2xl">
-                Left side: Landing Slogan alogn the lines: Powerful computer
-                vision from requests
-            </h1>
-            <h2>
-                [Potentially on right side] Small interactive example, e.g.
-                upload photo and get superscale
+        <div
+            class="
+                max-w-7xl
+                mx-auto
+                text-center
+                py-12
+                px-4
+                sm:px-6
+                lg:py-16
+                lg:px-8
+            "
+        >
+            <h2
+                class="
+                    text-3xl
+                    font-extrabold
+                    tracking-tight
+                    text-gray-200
+                    sm:text-4xl
+                "
+            >
+                <span class="block"
+                    >Left side: Landing Slogan alogn the lines: Powerful
+                    computer vision from requests</span
+                >
+                <span class="block"
+                    >[Potentially on right side] Small interactive example, e.g.
+                    upload photo and get superscale</span
+                >
             </h2>
         </div>
     </section>
