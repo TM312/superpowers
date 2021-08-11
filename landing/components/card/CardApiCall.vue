89f061b7b4fb8be15b41f2f1978903ac5a32d091
Thomas <thomas.moellers@rwth-aachen.de>
2021-08-11 09:30:34 +0800

    fix: Keep rounded in sm

--- landing/components/card/CardApiCall.vue
+++ landing/components/card/CardApiCall.vue
@@ -1,6 +1,6 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-    <div class="bg-gray-300 shadow sm:rounded-lg">
+    <div class="bg-gray-300 shadow rounded sm:rounded-lg">
         <div class="px-4 py-5 sm:p-6">
             <h3 class="text-lg leading-6 font-medium text-gray-900">
                 Continuous Integration
