e559924565d8dcdb58167bf32bb540844d0f60ed
Thomas <thomas.moellers@rwth-aachen.de>
2021-08-13 06:18:38 +0800

    fix: Make API call page dynamic

--- landing/components/section/requests/SectionRequestsDetails.vue
+++ landing/components/section/requests/SectionRequestsDetails.vue
@@ -2,12 +2,14 @@
 <template>
     <div class="bg-gray-700 shadow overflow-hidden sm:rounded-lg">
         <div class="px-4 py-5 sm:px-6">
-            <h3 class="text-lg leading-6 font-medium text-gray-200">
-                Applicant Information
-            </h3>
-            <p class="mt-1 max-w-2xl text-sm text-gray-400">
-                Personal details and application.
-            </p>
+            <h3
+                class="text-lg leading-6 font-medium text-gray-200"
+                v-text="request.name"
+            />
+            <p
+                class="mt-1 max-w-2xl text-sm text-gray-400"
+                v-text="request.description"
+            />
         </div>
         <div class="border-t border-gray-900 px-4 py-5 sm:px-6">
             <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
@@ -49,7 +51,7 @@
                 </div>
                 <div class="sm:col-span-2">
                     <dt class="text-sm font-medium text-gray-400">
-                        Attachments
+                        Sample Request
                     </dt>
                     <dd class="mt-1 text-sm text-gray-200">
                         <ul
@@ -129,6 +131,12 @@
 <script>
     export default {
         name: "SectionRequestsDetails",
+        props: {
+            request: {
+                type: Object,
+                required: true,
+            },
+        },
     };
 </script>
 
