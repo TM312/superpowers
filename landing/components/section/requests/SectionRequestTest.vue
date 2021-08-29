81c83141cce193630ccac577770d5ef936e20eb0
Thomas <thomas.moellers@rwth-aachen.de>
2021-08-29 16:16:18 +0800

    fix: Add test request section

---
+++ landing/components/section/requests/SectionRequestTest.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <container-request-test
+            :type="request.test_box_type"
+            :service_name="request.service_name"
+        />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SectionRequestTest",
+        props: {
+            request: {
+                type: Object,
+                required: true,
+            },
+        },
+    };
+</script>
+
+<style>
+</style>
